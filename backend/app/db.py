da625f12e2dddce3666dab1b924149fec53553f8
smukherjee <sujoym@gmail.com>
2026-05-23 13:41:29 +0530

    removed hardcodin of db url in db.py

--- backend/app/db.py
+++ backend/app/db.py
@@ -9,9 +9,7 @@ load_dotenv()
 
 # Example:
 # - Local Postgres: postgresql://postgres:<password>@127.0.0.1:5432/postgres
-# - Supabase: postgresql://postgres:<password>@<project-ref>.supabase.co:5432/postgres
 DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@127.0.0.1:54322/postgres")
-DATABASE_URL = "postgresql://postgres.dlwjwyfcyduowqvxcrrr:{password}@aws-1-ap-northeast-1.pooler.supabase.com:5432/postgres"
 
 engine = create_engine(DATABASE_URL, echo=True, future=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
